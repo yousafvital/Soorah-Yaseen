6d8512b8e8b6beea02950afde70bbbe2be3180b8
DESKTOP-D3G7FB7\HP <muhammaddanishriaz@gmail.com>
2024-11-25 21:42:38 +0500

    2nd

--- lib/Screens/ayat_by_ayat.dart
+++ lib/Screens/ayat_by_ayat.dart
@@ -100,128 +100,146 @@ class _AyatByAyatScreenState extends State<AyatByAyatScreen>
         ),
         centerTitle: true,
       ),
-      body: isLoading
-          ? const Center(child: CircularProgressIndicator())
-          : hasError
-              ? const Center(
-                  child: Text(
-                    "Failed to load data. Please check your file and try again.",
-                    style: TextStyle(fontSize: 16, color: Colors.red),
-                    textAlign: TextAlign.center,
-                  ),
-                )
-              : AnimationLimiter(
-                  child: ListView.builder(
-                    itemCount: ayatData.length,
-                    itemBuilder: (context, index) {
-                      final ayat = ayatData[index];
-                      final isSelected = index == playingIndex;
-                      final isCardExpanded = isExpanded[index] ?? false;
-                      final icon = isCardExpanded
-                          ? Icons.arrow_drop_up
-                          : Icons.arrow_drop_down;
+      body: Stack(
+        children: [
+          // Background image
+          Positioned.fill(
+            child: Image.asset(
+              'assets/images/background3.png',
+              fit: BoxFit.contain,
+            ),
+          ),
+          // Main content
+          isLoading
+              ? const Center(child: CircularProgressIndicator())
+              : hasError
+                  ? const Center(
+                      child: Text(
+                        "Failed to load data. Please check your file and try again.",
+                        style: TextStyle(fontSize: 16, color: Colors.red),
+                        textAlign: TextAlign.center,
+                      ),
+                    )
+                  : AnimationLimiter(
+                      child: ListView.builder(
+                        itemCount: ayatData.length,
+                        itemBuilder: (context, index) {
+                          final ayat = ayatData[index];
+                          final isSelected = index == playingIndex;
+                          final isCardExpanded = isExpanded[index] ?? false;
+                          final icon = isCardExpanded
+                              ? Icons.arrow_drop_up
+                              : Icons.arrow_drop_down;
 
-                      return AnimationConfiguration.staggeredList(
-                        position: index,
-                        duration: const Duration(milliseconds: 375),
-                        child: SlideAnimation(
-                          verticalOffset: 44.0,
-                          child: FadeInAnimation(
-                            child: InkWell(
-                              onTap: () {
-                                final audioPath =
-                                    'assets/audios/ayat_audios/m${ayat['id']}.mp3';
-                                toggleTafsirAndPlayAudio(index, audioPath);
-                              },
-                              child: Card(
-                                color: isSelected
-                                    ? const Color.fromARGB(252, 16, 16, 16)
-                                    : Colors.white,
-                                margin: const EdgeInsets.symmetric(
-                                    horizontal: 16, vertical: 8),
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(10),
-                                ),
-                                elevation: 4,
-                                child: Padding(
-                                  padding: const EdgeInsets.all(16.0),
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.stretch,
-                                    children: [
-                                      Text(
-                                        ayat['text'] ?? '',
-                                        textAlign: TextAlign.right,
-                                        style: const TextStyle(
-                                            fontSize: 20,
-                                            fontWeight: FontWeight.bold,
-                                            fontFamily: 'Quran'),
-                                      ),
-                                      const SizedBox(height: 10),
-                                      Text(
-                                        ayat['translate'] ?? '',
-                                        textAlign: TextAlign.right,
-                                        style: const TextStyle(
-                                            fontSize: 18,
-                                            color: Color.fromARGB(
-                                                255, 48, 90, 110),
-                                            fontFamily: 'Noori'),
-                                      ),
-                                      const SizedBox(height: 10),
-                                      AnimatedSize(
-                                        duration:
-                                            const Duration(milliseconds: 300),
-                                        curve: Curves.easeInOut,
-                                        child: isCardExpanded &&
-                                                ayat['tafsir'] != null &&
-                                                ayat['tafsir'].isNotEmpty
-                                            ? Column(
-                                                children: [
-                                                  Text(
-                                                    ayat['tafsir'] ?? '',
-                                                    textAlign: TextAlign.right,
-                                                    style: const TextStyle(
-                                                        fontSize: 16,
-                                                        color: Colors.black87,
-                                                        fontFamily: 'Noori'),
+                          return AnimationConfiguration.staggeredList(
+                            position: index,
+                            duration: const Duration(milliseconds: 375),
+                            child: SlideAnimation(
+                              verticalOffset: 44.0,
+                              child: FadeInAnimation(
+                                child: InkWell(
+                                  onTap: () {
+                                    final audioPath =
+                                        'assets/audios/ayat_audios/m${ayat['id']}.mp3';
+                                    toggleTafsirAndPlayAudio(index, audioPath);
+                                  },
+                                  child: Card(
+                                    color: isSelected
+                                        ? const Color.fromARGB(
+                                            250, 231, 233, 196)
+                                        : const Color.fromARGB(
+                                            222, 255, 255, 255),
+                                    margin: const EdgeInsets.symmetric(
+                                        horizontal: 16, vertical: 8),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10),
+                                    ),
+                                    elevation: 4,
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(16.0),
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.stretch,
+                                        children: [
+                                          Text(
+                                            ayat['text'] ?? '',
+                                            textAlign: TextAlign.right,
+                                            style: const TextStyle(
+                                                fontSize: 20,
+                                                fontWeight: FontWeight.bold,
+                                                fontFamily: 'Quran'),
+                                          ),
+                                          const SizedBox(height: 10),
+                                          Text(
+                                            ayat['translate'] ?? '',
+                                            textAlign: TextAlign.right,
+                                            style: const TextStyle(
+                                                fontSize: 18,
+                                                color: Color.fromARGB(
+                                                    255, 48, 90, 110),
+                                                fontFamily: 'Noori'),
+                                          ),
+                                          const SizedBox(height: 10),
+                                          AnimatedSize(
+                                            duration: const Duration(
+                                                milliseconds: 300),
+                                            curve: Curves.easeInOut,
+                                            child: isCardExpanded &&
+                                                    ayat['tafsir'] != null &&
+                                                    ayat['tafsir'].isNotEmpty
+                                                ? Column(
+                                                    children: [
+                                                      Text(
+                                                        ayat['tafsir'] ?? '',
+                                                        textAlign:
+                                                            TextAlign.right,
+                                                        style: const TextStyle(
+                                                            fontSize: 16,
+                                                            color:
+                                                                Colors.black87,
+                                                            fontFamily:
+                                                                'Noori'),
+                                                      ),
+                                                      const SizedBox(
+                                                          height: 10),
+                                                    ],
+                                                  )
+                                                : const SizedBox.shrink(),
+                                          ),
+                                          const SizedBox(height: 10),
+                                          Align(
+                                            alignment: Alignment.bottomRight,
+                                            child: Row(
+                                              mainAxisSize: MainAxisSize.min,
+                                              children: [
+                                                Icon(icon,
+                                                    color: isSelected
+                                                        ? Colors.green
+                                                        : Colors.grey),
+                                                const SizedBox(width: 4),
+                                                Text(
+                                                  ayat['id']?.toString() ?? '',
+                                                  style: const TextStyle(
+                                                    fontSize: 14,
+                                                    fontWeight: FontWeight.w300,
                                                   ),
-                                                  const SizedBox(height: 10),
-                                                ],
-                                              )
-                                            : const SizedBox.shrink(),
-                                      ),
-                                      const SizedBox(height: 10),
-                                      Align(
-                                        alignment: Alignment.bottomRight,
-                                        child: Row(
-                                          mainAxisSize: MainAxisSize.min,
-                                          children: [
-                                            Icon(icon,
-                                                color: isSelected
-                                                    ? Colors.green
-                                                    : Colors.grey),
-                                            const SizedBox(width: 4),
-                                            Text(
-                                              ayat['id']?.toString() ?? '',
-                                              style: const TextStyle(
-                                                fontSize: 14,
-                                                fontWeight: FontWeight.w300,
-                                              ),
+                                                ),
+                                              ],
                                             ),
-                                          ],
-                                        ),
+                                          ),
+                                        ],
                                       ),
-                                    ],
+                                    ),
                                   ),
                                 ),
                               ),
                             ),
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-                ),
+                          );
+                        },
+                      ),
+                    ),
+        ],
+      ),
     );
   }
 }
